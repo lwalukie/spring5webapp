48443c6807196d68b8803da8f4f2ab403ab08acd
Lukasz <lwalukiewicz@drfirst.com>
2022-11-16 12:57:25 +0100

    Introduced thymeleaf templates.

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Publisher publisher = new Publisher("Lukasz", "Lower Silesia", "Nadolice Wielkie", "Czernica", "55-003");
+        publisherRepository.save(publisher);
+
+        Author eric = new Author("Eric", "Evans");
+        Book book = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(book);
+        book.getAuthors().add(eric);
+
+        book.setPublisher(publisher);
+        publisher.getBooks().add(book);
+
+        authorRepository.save(eric);
+        bookRepository.save(book);
+        publisherRepository.save(publisher);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("JEE Development without EJB", "12321232123");
+        rod.getBooks().add(noEJB);
+        book.getAuthors().add(rod);
+
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of Books publisher: " + publisher.getBooks().size());
+    }
+}
